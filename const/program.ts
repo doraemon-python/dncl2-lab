3752b9b8de38e7973969824f8ee065c528aba09b
doraemon-python <96709349+doraemon-python@users.noreply.github.com>
2023-12-19 20:16:20 +0900

    line idをランダムなものに変更

--- const/program.ts
+++ const/program.ts
@@ -1,16 +1,22 @@
+import generateRandomId from "@/functions/generateRandomId";
 import { DNCL2Program } from "@/types/program";
+import { LINE_ID_LENGTH } from ".";
+
+const getId = () => {
+    return generateRandomId(LINE_ID_LENGTH);
+}
 
 const program: DNCL2Program = [
-    { lineId: "line1", type: "assign-variable", target: { name: "判定する数", id: "var1" }, value: { type: "number", value: "4" } },
-    { lineId: "line2", type: "assign-variable", target: { name: "割る数", id: "var2" }, value: { type: "number", value: "2" } },
-    { lineId: "few", type: "assign-variable", target: { name: "素数か", id: "var3" }, value: { type: "boolean", value: "true" } },
+    { lineId: getId(), type: "assign-variable", target: { name: "判定する数", id: "var1" }, value: { type: "number", value: "4" } },
+    { lineId: getId(), type: "assign-variable", target: { name: "割る数", id: "var2" }, value: { type: "number", value: "2" } },
+    { lineId: getId(), type: "assign-variable", target: { name: "素数か", id: "var3" }, value: { type: "boolean", value: "true" } },
     {
-        lineId: "line3", type: "while", condition: { operation: "smaller", values: [{ operation: "variable", id: "var2" }, { operation: "variable", id: "var1" }] }, lines: [
-            { lineId: "line3-1", type: "assign-variable", target: { name: "変数", id: "var4" }, value: { operation: "divide", values: [{ operation: "variable", id: "var1" }, { operation: "variable", id: "var2" }] } },
+        lineId: getId(), type: "while", condition: { operation: "smaller", values: [{ operation: "variable", id: "var2" }, { operation: "variable", id: "var1" }] }, lines: [
+            { lineId: getId(), type: "assign-variable", target: { name: "変数", id: "var4" }, value: { operation: "divide", values: [{ operation: "variable", id: "var1" }, { operation: "variable", id: "var2" }] } },
             {
                 type: "branch",
                 if: {
-                    ifId: "line3-2-if",
+                    ifId: getId(),
                     condition: {
                         operation: "equal",
                         values: [
@@ -19,13 +25,13 @@ const program: DNCL2Program = [
                         ]
                     },
                     lines: [
-                        { lineId: "line3-2-1", type: "reassign-variable", target: { id: "var3" }, value: { type: "boolean", value: "false" } },
-                        { lineId: "line3-2-2", type: "break" },
+                        { lineId: getId(), type: "reassign-variable", target: { id: "var3" }, value: { type: "boolean", value: "false" } },
+                        { lineId: getId(), type: "break" },
                     ]
                 },
                 else: {
-                    elseId: "line3-2-else",
-                    lines: [{ lineId: "line3-2-2", type: "reassign-variable", target: { id: "var2" }, value: { operation: "add", values: [{ operation: "variable", id: "var2" }, { type: "number", value: "1" }] } }]
+                    elseId: getId(),
+                    lines: [{ lineId: getId(), type: "reassign-variable", target: { id: "var2" }, value: { operation: "add", values: [{ operation: "variable", id: "var2" }, { type: "number", value: "1" }] } }]
                 }
             },
         ]
@@ -33,18 +39,18 @@ const program: DNCL2Program = [
     {
         type: "branch",
         if: {
-            ifId: "line6-1-if",
+            ifId: getId(),
             condition: { operation: "variable", id: "var3" },
             lines: [
                 // 表示するの例
-                { lineId: "line6-1", type: "function", id: "show", arg: { type: "string", value: "素数です。" } },
+                { lineId: getId(), type: "function", id: "show", arg: { type: "string", value: "素数です。" } },
             ]
         },
         else: {
-            elseId: "line6-1-else",
+            elseId: getId(),
             lines: [
-                { lineId: "line6-2", type: "function", id: "show", arg: { type: "string", value: "素数ではありません。" } },
-                { lineId: "line6-3", type: "function", id: "show", arg: { operation: "add", values: [{ operation: "function", id: "str", arg: { operation: "variable", id: "var2" } }, { type: "string", value: "で割り切れます。" }] } },
+                { lineId: getId(), type: "function", id: "show", arg: { type: "string", value: "素数ではありません。" } },
+                { lineId: getId(), type: "function", id: "show", arg: { operation: "add", values: [{ operation: "function", id: "str", arg: { operation: "variable", id: "var2" } }, { type: "string", value: "で割り切れます。" }] } },
             ]
         }
     },
